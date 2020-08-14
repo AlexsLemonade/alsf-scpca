5d58762bbb9a6ff6f3d20e3b971fa6a933931f15
Kurt Wheeler <kurt.wheeler91@gmail.com>
2020-08-14 15:30:04 -0400

    Fix terraform warning.

--- aws/nextflow-security.tf
+++ aws/nextflow-security.tf
@@ -50,7 +50,7 @@ resource "aws_route_table" "nf_route_table" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.nf_gateway.id}"
+    gateway_id = aws_internet_gateway.nf_gateway.id
   }
 
   tags = var.default_tags
