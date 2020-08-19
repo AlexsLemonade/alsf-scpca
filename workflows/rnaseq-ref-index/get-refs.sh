bb1849b14128f72415aeb5aca35b97216adb9a6d
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2020-08-18 21:38:43 -0400

    Add reference fetching script

---
+++ workflows/rnaseq-ref-index/get-refs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+
+s3_base=s3://nextflow-ccdl-data/reference/homo_sapiens/GRCh38/ensembl-100
+
+# Get reference fasta files and sync to S3
+wget -N -P fasta -i fasta_ref_urls.txt
+aws s3 sync fasta $s3_base/fasta
+
+# get annotation files & sync
+wget -N -P annotation -i annotation_ref_urls.txt
+aws s3 sync annotation $s3_base/annotation
+
+
+
+
