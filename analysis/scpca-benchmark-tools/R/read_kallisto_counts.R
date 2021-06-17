8c87b09143c15e8279815d183cff8ef26971cabb
Ally Hawkins <ally.hawkins@ccdatalab.org>
2021-06-17 16:11:06 -0500

    delete functions that are part of scpcaTools

--- analysis/scpca-benchmark-tools/R/read_kallisto_counts.R
+++
@@ -1,8 +0,0 @@
-read_kallisto_counts <- function(base) {
-  counts <- Matrix::readMM(paste0(base,".mtx"))%>%
-    t() %>% # transpose to gene x cell orientation
-    as("dgCMatrix") # compress sparse matrix
-  dimnames(counts) <- list(readLines(paste0(base,".", "genes.txt")),
-                           readLines(paste0(base,".barcodes.txt")))
-  return(counts)
-}
