55b8ed4f9b20d0db53d108aba4f19ccd870d8c0e
Ally Hawkins <ally.hawkins@ccdatalab.org>
2021-07-02 16:33:28 -0500

    remove QC function

--- analysis/benchmarking-functions/R/addPerCellQC_mito.R
+++
@@ -1,7 +0,0 @@
-addPerCellQC_mito <- function(sce, mito = mito_genes){
-  # add per cell QC with mitochondrial genes separately for later comparisons
-  scater::addPerCellQC(
-    sce, 
-    subsets = list(mito = mito[mito %in% rownames(sce)])
-  )
-}
