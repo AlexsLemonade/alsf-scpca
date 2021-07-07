55b448d2cfebc8164fa4900c4f4461fd266c7846
Ally Hawkins <ally.hawkins@ccdatalab.org>
2021-07-07 14:14:28 -0500

    add cellQC function

--- analysis/benchmarking-functions/R/make_sce_list.R
+++ analysis/benchmarking-functions/R/make_sce_list.R
@@ -1,3 +1,10 @@
+addCellQC <- function(sce, mito, threshold = 0){
+  scater::addPerCellQC(sce, 
+                       subsets = list(mito = mito[mito %in% rownames(sce)]),
+                       threshold = threshold)
+}
+
+
 make_sce_list <- function(info_df, filename, mito){
   sce_list <- mapply(scpcaTools::import_quant_data,
                      info_df$data_dir,
@@ -7,10 +14,7 @@ make_sce_list <- function(info_df, filename, mito){
                      info_df$usa_mode,
                      info_df$filter) 
   sce_list <- sce_list %>%
-    purrr::map(
-      ~ scater::addPerCellQC(.x,
-                             subsets = list(mito = mito[mito %in% rownames(.x)]))
-    ) %>%
+    purrr::map(addCellQC, mito) %>%
     purrr::map(
       ~ scater::addPerFeatureQC(.x)
     )
