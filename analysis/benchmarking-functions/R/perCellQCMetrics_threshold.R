51f43d7a00d2cb5546cc2e609a7cdc0079c7c67b
Ally Hawkins <ally.hawkins@ccdatalab.org>
2021-07-07 16:16:55 -0500

    remove extraneous function

--- analysis/benchmarking-functions/R/perCellQCMetrics_threshold.R
+++
@@ -1,7 +0,0 @@
-perCellQCMetrics_threshold <- function(sce, mito = mito_genes, threshold){
-  # add per cell QC with mitochondrial genes separately for later comparisons
-  scater::perCellQCMetrics(
-    sce,
-    threshold = threshold
-  )
-}
