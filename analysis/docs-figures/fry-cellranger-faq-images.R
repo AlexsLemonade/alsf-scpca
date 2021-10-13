c41d19c791a2c19edaf8832cee1183924054ecb0
Ally Hawkins <ally.hawkins@ccdatalab.org>
2021-10-12 22:44:18 -0500

    remove color from bar and condense plots

--- analysis/docs-figures/fry-cellranger-faq-images.R
+++ analysis/docs-figures/fry-cellranger-faq-images.R
@@ -30,12 +30,10 @@ mito_genes <- mito_genes %>%
   unique()
 
 # grab library metadata from AWS 
-library_data_s3 <- 's3://ccdl-scpca-data/sample_info'
+metadata_file_s3 <- 's3://ccdl-scpca-data/sample_info/scpca-library-metadata.tsv'
 
 # grab library metadata from location in s3
-sync_call <- paste('aws s3 sync', library_data_s3, library_data_dir,
-                   '--exclude "*"',
-                   '--include "*scpca-library-metadata.tsv"')
+sync_call <- paste('aws s3 cp', metadata_file_s3, library_data_dir)
 system(sync_call, ignore.stdout = TRUE)
 
 # read in library metadata to grab SCPCL id later 
@@ -95,66 +93,37 @@ nuclei_coldata_common <- coldata_common %>%
   dplyr::filter(seq_unit == "Nuclei")
 
 # create combined UMI per cell plot 
-umi_cell <- ggplot(cell_coldata_common, aes(x = sum, color = tool)) + 
+umi_plot <- ggplot(coldata_common, aes(x = sum, color = tool)) + 
   geom_density() + 
   facet_wrap(~ plot_id) +
   scale_x_log10(labels = scales::label_number()) +
   theme_bw() + 
   theme(strip.text.x = element_text(size = 8),
-        strip.background = element_rect(fill = "#56B4E9"),
-        axis.text.x = element_text(size = 6)) +
-  labs(x = "",
+        axis.text.x = element_text(size = 6),
+        legend.title = element_blank(),
+        legend.position = "top") +
+  labs(x = expression(paste(Log[10], " total UMI per cell")),
        y = "") +
   scale_color_manual(values = c("#D55E00", "#0072B2"))
-
-umi_nuclei <- ggplot(nuclei_coldata_common, aes(x = sum, color = tool)) + 
-  geom_density() + 
-  facet_wrap(~ plot_id) +
-  scale_x_log10(labels = scales::label_number()) +
-  theme_bw() + 
-  theme(strip.text.x = element_text(size = 8),
-        strip.background = element_rect(fill = "#E69F00"),
-        axis.text.x = element_text(size = 6)) +
-  labs(x = expression(paste(Log[10], " Total UMI per cell")),
-       y = "") +
-  scale_color_manual(values = c("#D55E00", "#0072B2"))
-
-# combine plots and add combined legend 
-combined_umi_plot <- umi_cell + umi_nuclei + plot_layout(guides = "collect", nrow = 2) & 
-  theme(legend.position = "top", legend.title = element_blank())
 
 # write combined plot to pdf
-ggsave(umi_density, plot = combined_umi_plot, width = 8, height = 5)
+ggsave(umi_density, plot = umi_plot, width = 8, height = 5)
 
 # do the same thing for genes detected per cell 
-genes_cell <- ggplot(cell_coldata_common, aes(x = detected, color = tool)) + 
+genes_plot <- ggplot(coldata_common, aes(x = detected, color = tool)) + 
   geom_density() + 
   facet_wrap(~ plot_id) +
   scale_x_log10(labels = scales::label_number()) +
   theme_bw() + 
   theme(strip.text.x = element_text(size = 8),
-        strip.background = element_rect(fill = "#56B4E9"),
-        axis.text.x = element_text(size = 6)) +
-  labs(x = "",
-       y = "") +
-  scale_color_manual(values = c("#D55E00", "#0072B2"))
-
-genes_nuclei <- ggplot(nuclei_coldata_common, aes(x = detected, color = tool)) + 
-  geom_density() + 
-  facet_wrap(~ plot_id) +
-  scale_x_log10(labels = scales::label_number()) +
-  theme_bw() + 
-  theme(strip.text.x = element_text(size = 8),
-        strip.background = element_rect(fill = "#E69F00"),
-        axis.text.x = element_text(size = 6)) +
+        axis.text.x = element_text(size = 6),
+        legend.title = element_blank(),
+        legend.position = "top") +
   labs(x = expression(paste(Log[10], " total genes detected per cell")),
        y = "") +
   scale_color_manual(values = c("#D55E00", "#0072B2"))
 
-combined_gene_plot <- genes_cell + genes_nuclei + plot_layout(guides = "collect", nrow = 2) & 
-  theme(legend.position = "top", legend.title = element_blank())
-
-ggsave(gene_density, plot = combined_gene_plot, width = 8, height = 5)
+ggsave(gene_density, plot = genes_plot, width = 8, height = 5)
 
 ## Mean gene expression correlation  
 # grab rowdata from filtered sces
@@ -170,24 +139,12 @@ rowdata_df <- all_sces_list %>%
   dplyr::left_join(select_metadata_df, by = c("sample" = "scpca_run_id")) %>%
   dplyr::mutate(plot_id = paste(scpca_library_id, seq_unit, sep = "-"))
   
-# filter genes with low detection 
-gene_counts <- rowdata_df %>% 
-  # remove genes that have a low frequency of being detected
-  dplyr::filter(detected >= 5.0) %>%
-  dplyr::count(gene_id, sample)
-
-# filter to only genes that are in both tools
-common_genes <- gene_counts %>%
-  dplyr::filter(n == 2) %>%
-  dplyr::pull(gene_id)
-
-rowdata_df_common <- rowdata_df %>%
-  dplyr::filter(
-    (gene_id %in% common_genes) 
-  )
+# remove genes with low detection 
+rowdata_df <- rowdata_df %>%
+  dplyr::filter(detected >= 5.0)
 
 # spread table to put mean expression for Alevin-fry and Cell ranger in its own columns for plotting
-rowdata_cor <- rowdata_df_common %>%
+rowdata_cor <- rowdata_df %>%
   dplyr::select(tool, gene_id, plot_id, mean) %>%
   # spread the mean expression stats to one column per caller
   tidyr::pivot_wider(id_cols = c(gene_id, plot_id),
@@ -203,7 +160,7 @@ ggplot(rowdata_cor, aes(x = `Alevin-fry`, y = `Cell Ranger`)) +
   scale_x_log10(labels = scales::label_number()) + 
   scale_y_log10(labels = scales::label_number()) + 
   labs(x = "Cell Ranger mean gene expression", y = "Alevin-fry mean gene expression") + 
-  theme_classic() + 
+  theme_bw() + 
   theme(axis.text = element_text(size = 6),
         strip.background = element_rect())+
   stat_cor(aes(label = ..r.label..), method = "pearson", size = 2)
