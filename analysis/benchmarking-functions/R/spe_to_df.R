bd4cddd0594a14e55993c4112ded4afdbdc1551c
Ally Hawkins <ally.hawkins@ccdatalab.org>
2021-11-30 16:43:58 -0600

    remove rowdata_to_df

--- analysis/benchmarking-functions/R/spe_to_df.R
+++ analysis/benchmarking-functions/R/spe_to_df.R
@@ -11,16 +11,3 @@ spatial_coldata_to_df <- function(spe){
     tibble::rownames_to_column(var = "spot_id") %>%
     dplyr::mutate(in_tissue = spatialData(spe)$in_tissue)
 }
-
-
-#' Convert rowData from SpatialExperiment object into data.frame
-#'
-#' @param spe SpatialExperiment obejct with per gene QC metrics in rowData
-#'
-#' @return data.frame containing rowData from original spe
-spatial_rowdata_to_df <- function(spe){
-  
-  # grab rowdata and convert to data.frame
-  df <- as.data.frame(rowData(spe)) %>%
-    tibble::rownames_to_column(var = "gene_id")
-}
