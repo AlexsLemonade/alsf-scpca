43f4cb2d3e10488949c2fa59151b7ec84f035c83
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2022-05-12 17:26:08 -0400

    update script with correct file name

--- analysis/quantifier-comparisons/15a-render-demux.R
+++ analysis/quantifier-comparisons/15a-render-demux.R
@@ -1,8 +1,8 @@
 #!/usr/bin/env Rscript
 
-# Run cellhash demux notebook for each sample
+# Run cellhash demux notebook for each sample in the data folder
 
-rmd_template <- "15-cellhash-demux.Rmd"
+rmd_template <- "15-demux-comparisons.Rmd"
 outdir <- file.path("results", "notebooks")
 dir.create(outdir, recursive = TRUE, showWarnings = FALSE)
 # samples have odd lib ids between 533 and 567
