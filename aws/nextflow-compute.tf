fa2982bd95c0ba5324f4ee724f2c87a62f126e9d
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2024-04-04 15:16:32 -0400

    Revert "add temp bucket access and simplify policies"
    This reverts commit 196a7824389b228f56cb10edcb6247ae2834ba4e.

--- aws/nextflow-compute.tf
+++ aws/nextflow-compute.tf
@@ -12,7 +12,7 @@ resource "aws_iam_instance_profile" "nf_ecs_instance_role" {
 # the AMI used is described in setup-log.md
 resource "aws_batch_compute_environment" "nf_spot" {
   compute_environment_name = "nextflow-spot-compute"
-  tags                     = var.default_tags
+  tags = var.default_tags
   compute_resources {
     instance_role = aws_iam_instance_profile.nf_ecs_instance_role.arn
     instance_type = [
@@ -20,13 +20,13 @@ resource "aws_batch_compute_environment" "nf_spot" {
     ]
     allocation_strategy = "SPOT_PRICE_CAPACITY_OPTIMIZED"
     spot_iam_fleet_role = aws_iam_role.nf_spotfleet_role.arn
-    bid_percentage      = 100
-    max_vcpus           = 256
-    min_vcpus           = 0
+    bid_percentage = 100
+    max_vcpus = 256
+    min_vcpus = 0
     # standard launch template
     launch_template {
       launch_template_id = aws_launch_template.nf_lt_standard.id
-      version            = aws_launch_template.nf_lt_standard.latest_version
+      version = aws_launch_template.nf_lt_standard.latest_version
     }
     # ec2_key_pair = aws_key_pair.nf_keypair.key_name
     security_group_ids = [
@@ -53,7 +53,7 @@ resource "aws_batch_compute_environment" "nf_spot" {
 
 resource "aws_batch_compute_environment" "nf_spot_bigdisk" {
   compute_environment_name = "nextflow-spot-compute-bigdisk"
-  tags                     = var.default_tags
+  tags = var.default_tags
   compute_resources {
     instance_role = aws_iam_instance_profile.nf_ecs_instance_role.arn
     instance_type = [
@@ -61,13 +61,13 @@ resource "aws_batch_compute_environment" "nf_spot_bigdisk" {
     ]
     allocation_strategy = "SPOT_PRICE_CAPACITY_OPTIMIZED"
     spot_iam_fleet_role = aws_iam_role.nf_spotfleet_role.arn
-    bid_percentage      = 100
-    max_vcpus           = 128
-    min_vcpus           = 0
+    bid_percentage = 100
+    max_vcpus = 128
+    min_vcpus = 0
     # large disk launch template
     launch_template {
       launch_template_id = aws_launch_template.nf_lt_bigdisk.id
-      version            = aws_launch_template.nf_lt_bigdisk.latest_version
+      version = aws_launch_template.nf_lt_bigdisk.latest_version
     }
     # ec2_key_pair = aws_key_pair.nf_keypair.key_name
     security_group_ids = [
@@ -93,7 +93,7 @@ resource "aws_batch_compute_environment" "nf_spot_bigdisk" {
 # Create a spot instance environment with up to 2048 vcpus and auto-scaled EBS.
 resource "aws_batch_compute_environment" "nf_spot_auto_scaled_ebs" {
   compute_environment_name = "nextflow-spot-compute-auto-scaled-ebs"
-  tags                     = var.default_tags
+  tags = var.default_tags
   compute_resources {
     instance_role = aws_iam_instance_profile.nf_ecs_instance_role.arn
     instance_type = [
@@ -105,13 +105,13 @@ resource "aws_batch_compute_environment" "nf_spot_auto_scaled_ebs" {
     ]
     allocation_strategy = "SPOT_PRICE_CAPACITY_OPTIMIZED"
     spot_iam_fleet_role = aws_iam_role.nf_spotfleet_role.arn
-    bid_percentage      = 100
-    max_vcpus           = 2048
-    min_vcpus           = 0
+    bid_percentage = 100
+    max_vcpus = 2048
+    min_vcpus = 0
 
     launch_template {
       launch_template_id = aws_launch_template.nf_lt_auto_scaled_ebs.id
-      version            = aws_launch_template.nf_lt_auto_scaled_ebs.latest_version
+      version = aws_launch_template.nf_lt_auto_scaled_ebs.latest_version
     }
     security_group_ids = [
       aws_security_group.nf_security.id,
@@ -136,7 +136,7 @@ resource "aws_batch_compute_environment" "nf_spot_auto_scaled_ebs" {
 # Create an ondemand environment with up to 512 vcpus
 resource "aws_batch_compute_environment" "nf_ondemand" {
   compute_environment_name = "nextflow-ondemand-compute"
-  tags                     = var.default_tags
+  tags = var.default_tags
   compute_resources {
     instance_role = aws_iam_instance_profile.nf_ecs_instance_role.arn
     instance_type = [
@@ -147,12 +147,12 @@ resource "aws_batch_compute_environment" "nf_ondemand" {
       "c6a", "m6a", "r6a",
     ]
     allocation_strategy = "BEST_FIT"
-    max_vcpus           = 512
-    min_vcpus           = 0
+    max_vcpus = 512
+    min_vcpus = 0
     # standard launch template
     launch_template {
       launch_template_id = aws_launch_template.nf_lt_auto_scaled_ebs.id
-      version            = aws_launch_template.nf_lt_auto_scaled_ebs.latest_version
+      version = aws_launch_template.nf_lt_auto_scaled_ebs.latest_version
     }
     # ec2_key_pair = aws_key_pair.nf_keypair.key_name
     security_group_ids = [
--- aws/nextflow-policies.tf
+++ aws/nextflow-policies.tf
@@ -6,7 +6,7 @@
 # This policy allows read and write access to specific buckets for nextflow processing
 resource "aws_iam_policy" "nf_readwrite_S3" {
   name   = "nextflow-ccdl-readwrite-s3"
-  tags   = var.default_tags
+  tags = var.default_tags
   policy = <<EOF
 {
  "Version": "2012-10-17",
@@ -14,16 +14,76 @@ resource "aws_iam_policy" "nf_readwrite_S3" {
   {
    "Effect": "Allow",
    "Action": [
+    "s3:PutAnalyticsConfiguration",
+    "s3:GetObjectVersionTagging",
+    "s3:ReplicateObject",
+    "s3:GetObjectAcl",
+    "s3:GetBucketObjectLockConfiguration",
+    "s3:PutLifecycleConfiguration",
+    "s3:GetObjectVersionAcl",
+    "s3:PutObjectTagging",
+    "s3:DeleteObject",
+    "s3:DeleteObjectTagging",
+    "s3:GetBucketPolicyStatus",
+    "s3:GetObjectRetention",
+    "s3:GetBucketWebsite",
+    "s3:PutReplicationConfiguration",
+    "s3:DeleteObjectVersionTagging",
+    "s3:PutObjectLegalHold",
+    "s3:GetObjectLegalHold",
+    "s3:GetBucketNotification",
+    "s3:PutBucketCORS",
+    "s3:GetReplicationConfiguration",
+    "s3:ListMultipartUploadParts",
+    "s3:PutObject",
+    "s3:GetObject",
+    "s3:PutBucketNotification",
+    "s3:PutBucketLogging",
+    "s3:GetAnalyticsConfiguration",
+    "s3:PutBucketObjectLockConfiguration",
+    "s3:GetObjectVersionForReplication",
+    "s3:GetLifecycleConfiguration",
+    "s3:GetInventoryConfiguration",
+    "s3:GetBucketTagging",
+    "s3:PutAccelerateConfiguration",
+    "s3:DeleteObjectVersion",
+    "s3:GetBucketLogging",
+    "s3:ListBucketVersions",
+    "s3:ReplicateTags",
+    "s3:RestoreObject",
     "s3:ListBucket",
-    "s3:*Object"
+    "s3:GetAccelerateConfiguration",
+    "s3:GetBucketPolicy",
+    "s3:PutEncryptionConfiguration",
+    "s3:GetEncryptionConfiguration",
+    "s3:GetObjectVersionTorrent",
+    "s3:AbortMultipartUpload",
+    "s3:PutBucketTagging",
+    "s3:GetBucketRequestPayment",
+    "s3:GetObjectTagging",
+    "s3:GetMetricsConfiguration",
+    "s3:PutBucketVersioning",
+    "s3:GetBucketPublicAccessBlock",
+    "s3:ListBucketMultipartUploads",
+    "s3:PutMetricsConfiguration",
+    "s3:PutObjectVersionTagging",
+    "s3:GetBucketVersioning",
+    "s3:GetBucketAcl",
+    "s3:PutInventoryConfiguration",
+    "s3:GetObjectTorrent",
+    "s3:PutBucketWebsite",
+    "s3:PutBucketRequestPayment",
+    "s3:PutObjectRetention",
+    "s3:GetBucketCORS",
+    "s3:GetBucketLocation",
+    "s3:ReplicateDelete",
+    "s3:GetObjectVersion"
    ],
    "Resource": [
-    "arn:aws:s3:::nextflow-ccdl-data",
     "arn:aws:s3:::nextflow-ccdl-data/*",
-    "arn:aws:s3:::nextflow-ccdl-results",
     "arn:aws:s3:::nextflow-ccdl-results/*",
-    "arn:aws:s3:::openscpca-temp-simdata",
-    "arn:aws:s3:::openscpca-temp-simdata/*"
+    "arn:aws:s3:::nextflow-ccdl-data",
+    "arn:aws:s3:::nextflow-ccdl-results"
    ]
   },
   {
@@ -44,7 +104,7 @@ EOF
 # This policy gives read access to all S3 buckets
 resource "aws_iam_policy" "nf_read_S3" {
   name   = "nextflow-ccdl-read-s3"
-  tags   = var.default_tags
+  tags = var.default_tags
   policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -52,15 +112,22 @@ resource "aws_iam_policy" "nf_read_S3" {
     {
       "Effect": "Allow",
       "Action": [
-        "s3:GetObject*",
-        "s3:ListBucket"
+        "s3:GetObjectVersionTorrent",
+        "s3:GetObjectAcl",
+        "s3:GetObject",
+        "s3:GetObjectTorrent",
+        "s3:GetObjectRetention",
+        "s3:GetObjectVersionTagging",
+        "s3:GetObjectVersionAcl",
+        "s3:GetObjectTagging",
+        "s3:GetObjectVersionForReplication",
+        "s3:GetObjectLegalHold",
+        "s3:GetObjectVersion",
+        "s3:ListMultipartUploadParts"
       ],
       "Resource": [
-        "arn:aws:s3:::ccdl-scpca-data",
         "arn:aws:s3:::ccdl-scpca-data/*",
-        "arn:aws:s3:::nextflow-ccdl-data",
         "arn:aws:s3:::nextflow-ccdl-data/*",
-        "arn:aws:s3:::nextflow-ccdl-results",
         "arn:aws:s3:::nextflow-ccdl-results/*"
       ]
     },
@@ -120,10 +187,10 @@ EOF
 }
 
 resource "aws_iam_policy" "nf_manage_ebs" {
-  name        = "nextflow-ccdl-manage-ebs"
+  name   = "nextflow-ccdl-manage-ebs"
   description = "A policy that allows to manage (attach/create/delete) EBS volumes."
-  tags        = var.default_tags
-  policy      = <<EOF
+  tags = var.default_tags
+  policy = <<EOF
 {
     "Version": "2012-10-17",
     "Statement": [
