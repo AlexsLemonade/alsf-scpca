7e304334ddab34fbcaadf042e360c5a142db2ffc
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2026-07-06 15:38:43 -0400

    Comment out all nextflow resources

--- aws/nextflow-batch.tf
+++ aws/nextflow-batch.tf
@@ -1,56 +1,56 @@
 # AWS Batch setup
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
 variable "default_tags" {
   description = "Default resource tags"
   type        = map(string)
-  default     = {
-    team = "science"
+  default = {
+    team    = "science"
     project = "scpca"
     purpose = "nextflow-batch"
-    config = "https://github.com/AlexsLemonade/alsf-scpca/tree/main/aws"
+    config  = "https://github.com/AlexsLemonade/alsf-scpca/tree/main/aws"
   }
 
 }
 
-resource "aws_batch_job_queue" "nf_default_queue" {
-  name     = "nextflow-batch-default-queue"
-  tags     = var.default_tags
-  state    = "ENABLED"
-  priority = 1
-  compute_environments = [
-    aws_batch_compute_environment.nf_spot.arn,
-  ]
-}
+# resource "aws_batch_job_queue" "nf_default_queue" {
+#   name     = "nextflow-batch-default-queue"
+#   tags     = var.default_tags
+#   state    = "ENABLED"
+#   priority = 1
+#   compute_environments = [
+#     aws_batch_compute_environment.nf_spot.arn,
+#   ]
+# }
 
-resource "aws_batch_job_queue" "nf_bigdisk_queue" {
-  name     = "nextflow-batch-bigdisk-queue"
-  tags     = var.default_tags
-  state    = "ENABLED"
-  priority = 1
-  compute_environments = [
-    aws_batch_compute_environment.nf_spot_bigdisk.arn,
-  ]
-}
+# resource "aws_batch_job_queue" "nf_bigdisk_queue" {
+#   name     = "nextflow-batch-bigdisk-queue"
+#   tags     = var.default_tags
+#   state    = "ENABLED"
+#   priority = 1
+#   compute_environments = [
+#     aws_batch_compute_environment.nf_spot_bigdisk.arn,
+#   ]
+# }
 
-resource "aws_batch_job_queue" "nf_autoscale_queue" {
-  name = "nextflow-batch-autoscale-queue"
-  tags = var.default_tags
-  state = "ENABLED"
-  priority = 1
-  compute_environments = [
-    aws_batch_compute_environment.nf_spot_auto_scaled_ebs.arn,
-  ]
-}
+# resource "aws_batch_job_queue" "nf_autoscale_queue" {
+#   name = "nextflow-batch-autoscale-queue"
+#   tags = var.default_tags
+#   state = "ENABLED"
+#   priority = 1
+#   compute_environments = [
+#     aws_batch_compute_environment.nf_spot_auto_scaled_ebs.arn,
+#   ]
+# }
 
 
-resource "aws_batch_job_queue" "nf_priority_queue" {
-  name     = "nextflow-batch-priority-queue"
-  state    = "ENABLED"
-  priority = 1
-  compute_environments = [
-    aws_batch_compute_environment.nf_ondemand.arn,
-  ]
-}
+# resource "aws_batch_job_queue" "nf_priority_queue" {
+#   name     = "nextflow-batch-priority-queue"
+#   state    = "ENABLED"
+#   priority = 1
+#   compute_environments = [
+#     aws_batch_compute_environment.nf_ondemand.arn,
+#   ]
+# }
--- aws/nextflow-launch-templates.tf
+++ aws/nextflow-launch-templates.tf
@@ -1,48 +1,48 @@
-resource "aws_launch_template" "nf_lt_standard" {
-  name = "nextflow-launchtemplate-standard"
-  tags = var.default_tags
-  # ccdl-nextflow-base-v2.0 image
-  image_id = "ami-01c08d4de548477df"
-  block_device_mappings {
-    device_name = "/dev/xvda"
-    ebs {
-      volume_size = 128
-      encrypted = true
-      delete_on_termination = true
-    }
-  }
-  update_default_version = true
-}
+# resource "aws_launch_template" "nf_lt_standard" {
+#   name = "nextflow-launchtemplate-standard"
+#   tags = var.default_tags
+#   # ccdl-nextflow-base-v2.0 image
+#   image_id = "ami-01c08d4de548477df"
+#   block_device_mappings {
+#     device_name = "/dev/xvda"
+#     ebs {
+#       volume_size = 128
+#       encrypted = true
+#       delete_on_termination = true
+#     }
+#   }
+#   update_default_version = true
+# }
 
-resource "aws_launch_template" "nf_lt_bigdisk" {
-  name = "nextflow-launchtemplate-bigdisk"
-  tags = var.default_tags
-  # ccdl-nextflow-base-v2.0 image
-  image_id = "ami-01c08d4de548477df"
-  block_device_mappings {
-    device_name = "/dev/xvda"
-    ebs {
-      volume_size = 1000
-      encrypted = true
-      delete_on_termination = true
-    }
-  }
-  update_default_version = true
-}
+# resource "aws_launch_template" "nf_lt_bigdisk" {
+#   name = "nextflow-launchtemplate-bigdisk"
+#   tags = var.default_tags
+#   # ccdl-nextflow-base-v2.0 image
+#   image_id = "ami-01c08d4de548477df"
+#   block_device_mappings {
+#     device_name = "/dev/xvda"
+#     ebs {
+#       volume_size = 1000
+#       encrypted = true
+#       delete_on_termination = true
+#     }
+#   }
+#   update_default_version = true
+# }
 
-resource "aws_launch_template" "nf_lt_auto_scaled_ebs" {
-  name = "nextflow-launchtemplate-auto-scaled-ebs"
-  tags = var.default_tags
-  image_id = "ami-01c08d4de548477df"  # ccdl-nextflow-base-v2.0 image
-  block_device_mappings {
-    device_name = "/dev/xvda"
-    ebs {
-      volume_size = 30  # GiB
-      volume_type = "gp3"
-      encrypted = true
-      delete_on_termination = true
-    }
-  }
-  update_default_version = true
-  user_data = filebase64("./user_data/auto_scaled_ebs")
-}
+# resource "aws_launch_template" "nf_lt_auto_scaled_ebs" {
+#   name = "nextflow-launchtemplate-auto-scaled-ebs"
+#   tags = var.default_tags
+#   image_id = "ami-01c08d4de548477df"  # ccdl-nextflow-base-v2.0 image
+#   block_device_mappings {
+#     device_name = "/dev/xvda"
+#     ebs {
+#       volume_size = 30  # GiB
+#       volume_type = "gp3"
+#       encrypted = true
+#       delete_on_termination = true
+#     }
+#   }
+#   update_default_version = true
+#   user_data = filebase64("./user_data/auto_scaled_ebs")
+# }
